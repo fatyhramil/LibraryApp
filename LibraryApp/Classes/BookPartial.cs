60d0c949daf539aaf70847ce42db057ea645fd92
Ramil <ramilfatykhov02@gmail.com>
2022-05-20 13:55:38 +0300

    first commit

---
+++ LibraryApp/Classes/BookPartial.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace LibraryApp
+{
+    public partial class Book
+    {
+        public string FullImagePath
+        {
+            get
+            {
+                if (PhotoName != null)
+                {
+                    return "/Images/" + PhotoName;
+                }
+                else
+                {
+                    return "/Images/picture.png";
+                }
+                
+            }
+        }
+        public SolidColorBrush DiscountColor
+        {
+            get
+            {
+                if (CurrentDiscount >= 15)
+                {
+                    return (SolidColorBrush)new BrushConverter().ConvertFrom("#7fff00");
+                }
+                else
+                {
+                    return Brushes.White;
+                }
+            }
+        }
+        public string FinalPrice
+        {
+            get
+            {
+                if (CurrentDiscount != 0 && CurrentDiscount != null)
+                {
+                    string price = Price.ToString()+"  ";
+                
+                    return price+(Price - ((decimal)(CurrentDiscount * 0.01) * Price)).ToString();
+                }
+                else
+                {
+                    return Price.ToString();
+                }
+            }
+        }
+        public Visibility LineVisibility
+        {
+            get
+            {
+                if (CurrentDiscount == null || CurrentDiscount == 0)
+                {
+                    return Visibility.Hidden;
+                }
+                else
+                {
+                    return Visibility.Visible;
+                }
+            }
+        }
+    }
+}
